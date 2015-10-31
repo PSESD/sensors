0dfebf0cd33c0f7011c5f31da2dc709a1b7c4b66
Jake Oehler Morrison <jmorrison@psesd.org>
2015-10-31 16:22:43 -0700

    initial sensor set

---
+++ lib/Extension.php
@@ -0,0 +1,13 @@
+<?php
+namespace canis\sensor\Extension;
+
+class Extension implements \yii\base\BootstrapInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function bootstrap($app)
+    {
+        Yii::setAlias('@canis/sensor', __DIR__);
+    }
+}
---
+++ lib/sites/Base.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @link http://canis.io
+ *
+ * @copyright Copyright (c) 2015 Canis
+ * @license http://canis.io/license/
+ */
+
+namespace canis\sensors\sites;
+
+abstract class Base extends \yii\base\Object
+{
+
+}
