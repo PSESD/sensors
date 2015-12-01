6fc035bf684fd72ee7ecd50ba58d1a7c681d60af
Jake Oehler Morrison <jmorrison@psesd.org>
2015-12-01 15:27:45 -0800

    id on local sensors

--- lib/local/Sensor.php
+++ lib/local/Sensor.php
@@ -12,6 +12,17 @@ use Yii;
 
 abstract class Sensor extends \canis\sensors\base\Sensor
 {
+	protected $_id;
+	public function setId($id)
+	{
+		$this->_id = $id;
+	}
+
+	public function getId()
+	{
+		return $this->_id;
+	}
+
 	public function getCheckInterval()
 	{
 		return false;
